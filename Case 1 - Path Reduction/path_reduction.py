34f07aed7bc117a31e35055ec3be64c2cf365661
Aušrinė M <ausre.makauskaite@gmail.com>
2022-11-21 21:15:08 +0200

    auto-formatted code

--- Case 1 - Path Reduction/path_reduction.py
+++ Case 1 - Path Reduction/path_reduction.py
@@ -1,17 +1,22 @@
 def pathReduc(ls):
-    needless_paths = [["NORTH", "SOUTH"], ["SOUTH", "NORTH"], ["EAST", "WEST"], ["WEST", "EAST"]]
+    needless_paths = [
+        ["NORTH", "SOUTH"],
+        ["SOUTH", "NORTH"],
+        ["EAST", "WEST"],
+        ["WEST", "EAST"],
+    ]
 
     ix = 0
-    while ix < len(ls)-1: #we don't need to check the last item in the list as it will be checked with the item before it
-        if ls[ix] == ls[ix+1]:
-            ix+=1
+    while ix < len(ls) - 1:
+        if ls[ix] == ls[ix + 1]:
+            ix += 1
         else:
-            if [ls[ix], ls[ix+1]] in needless_paths:
-                ls.pop(ix+1)
+            if [ls[ix], ls[ix + 1]] in needless_paths:
+                ls.pop(ix + 1)
                 ls.pop(ix)
-                if(ix!=0):
-                    ix-=1
+                if ix != 0:
+                    ix -= 1
             else:
-                ix+=1
+                ix += 1
 
     return ls
--- Case 1 - Path Reduction/test_path_reduction.py
+++ Case 1 - Path Reduction/test_path_reduction.py
@@ -1,22 +1,44 @@
 import path_reduction as pr
 
-def test_pathReduc():
-    assert pr.pathReduc(["NORTH","SOUTH","SOUTH","EAST","WEST","NORTH","WEST"]) == ["WEST"], 'Should be ["WEST"]'
-    assert pr.pathReduc(["NORTH","SOUTH","SOUTH","EAST","WEST","NORTH"]) == [], 'Should be []'
-    assert pr.pathReduc(["NORTH","WEST","SOUTH","EAST"]) == ["NORTH","WEST","SOUTH","EAST"], 'Should be ["NORTH","WEST","SOUTH","EAST"]'
-    assert pr.pathReduc(["NORTH", "EAST", "WEST", "SOUTH", "WEST", "WEST"]) == ["WEST", "WEST"], 'Should be ["WEST", "WEST"]'
 
-    
+def test_pathReduc():
+    assert pr.pathReduc(
+        ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]
+    ) == ["WEST"], 'Should be ["WEST"]'
+    assert (
+        pr.pathReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH"]) == []
+    ), "Should be []"
+    assert pr.pathReduc(["NORTH", "WEST", "SOUTH", "EAST"]) == [
+        "NORTH",
+        "WEST",
+        "SOUTH",
+        "EAST",
+    ], 'Should be ["NORTH","WEST","SOUTH","EAST"]'
+    assert pr.pathReduc(["NORTH", "EAST", "WEST", "SOUTH", "WEST", "WEST"]) == [
+        "WEST",
+        "WEST",
+    ], 'Should be ["WEST", "WEST"]'
+
+
 def test_pathReduc_corner_cases():
-    assert pr.pathReduc([]) == [], 'Should be []'
-    assert pr.pathReduc(["NORTH", "SOUTH", "SOUTH", "NORTH"]) == [], 'Should be []'
-    assert pr.pathReduc(["NORTH", "SOUTH", "WEST" ,"NORTH", "SOUTH"]) == ["WEST"], 'Should be ["WEST"]'
+    assert pr.pathReduc([]) == [], "Should be []"
+    assert pr.pathReduc(["NORTH", "SOUTH", "SOUTH", "NORTH"]) == [], "Should be []"
+    assert pr.pathReduc(["NORTH", "SOUTH", "WEST", "NORTH", "SOUTH"]) == [
+        "WEST"
+    ], 'Should be ["WEST"]'
     assert pr.pathReduc(["EAST"]) == ["EAST"], 'Should be ["EAST"]'
-    assert pr.pathReduc(["EAST", "EAST","EAST","EAST"]) == ["EAST", "EAST","EAST","EAST"], 'Should be ["EAST", "EAST","EAST","EAST"]'
+    assert pr.pathReduc(["EAST", "EAST", "EAST", "EAST"]) == [
+        "EAST",
+        "EAST",
+        "EAST",
+        "EAST",
+    ], 'Should be ["EAST", "EAST","EAST","EAST"]'
+
 
 def run_all_tests():
     test_pathReduc()
     test_pathReduc_corner_cases()
     print("All tests passed")
+
 
 run_all_tests()
