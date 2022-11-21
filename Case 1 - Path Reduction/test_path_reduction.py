46eebbe7019080d65e59f438e0b9c58e6349fa8b
Aušrinė M <ausre.makauskaite@gmail.com>
2022-11-21 20:49:42 +0200

    Running all test cases added

--- Case 1 - Path Reduction/test_path_reduction.py
+++ Case 1 - Path Reduction/test_path_reduction.py
@@ -1,7 +1,12 @@
-from xml.etree.ElementPath import prepare_child
 import path_reduction as pr
 
 def test_pathReduc():
     assert pr.pathReduc(["NORTH","SOUTH","SOUTH","EAST","WEST","NORTH","WEST"]) == ["WEST"], 'Should be ["WEST"]'
     assert pr.pathReduc(["NORTH","SOUTH","SOUTH","EAST","WEST","NORTH"]) == [], 'Should be []'
     assert pr.pathReduc(["NORTH","WEST","SOUTH","EAST"]) == ["NORTH","WEST","SOUTH","EAST"], 'Should be ["NORTH","WEST","SOUTH","EAST"]'
+
+def run_all_tests():
+    test_pathReduc()
+    print("All tests passed")
+
+run_all_tests()
