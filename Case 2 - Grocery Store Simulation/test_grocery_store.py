23cc3379bc2b028baf1fb9982fb46ffcb23a870e
Aušrinė M <ausre.makauskaite@gmail.com>
2022-11-21 22:51:01 +0200

    Added test cases from Telia

--- Case 2 - Grocery Store Simulation/test_grocery_store.py
+++ Case 2 - Grocery Store Simulation/test_grocery_store.py
@@ -1,7 +1,9 @@
 import grocery_store as gc
 
 def test_computeCheckoutTime():
-    print("testing")
+    assert gc.computeCheckoutTime([5,3,4], 1) == 12, "Should be 12"
+    assert gc.computeCheckoutTime([10,2,3,3], 2) == 10, "Should be 10"
+    assert gc.computeCheckoutTime([2,3,10], 2) == 12, "Should be 12"
 
 def run_all_tests():
     test_computeCheckoutTime()
